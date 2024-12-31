9987e74670bae7a1a246d09b5b6d0cc1b3f84ce5
qwillbert <ash@ashcode.dev>
2024-12-31 11:44:21 -0500

    Convert Indentation to tabs

--- server/src/rooms/GameRoom.ts
+++ server/src/rooms/GameRoom.ts
@@ -4,63 +4,63 @@ import { Schema, MapSchema, type } from "@colyseus/schema";
 import { generateUsername } from "unique-username-generator";
 
 export class GameRoom extends Room<GameRoomState> {
-  maxClients = 4;
+	maxClients = 4;
 
-  onCreate (options: any) {
-    this.setState(new GameRoomState({
-      time: 0,
-    }));
+	onCreate(options: any) {
+		this.setState(new GameRoomState({
+			time: 0,
+		}));
 
-    this.setPrivate(options.private || false);
+		this.setPrivate(options.private || false);
 
-    this.maxClients = Math.min(parseInt(options.maxPlayers), 99);
+		this.maxClients = Math.min(parseInt(options.maxPlayers), 99);
 
-    console.log("GameRoom created!", options);
+		console.log("GameRoom created!", options);
 
-    this.onMessage("move", (client, message) => {
-      const player = this.state.players.get(client.sessionId);
+		this.onMessage("move", (client, message) => {
+			const player = this.state.players.get(client.sessionId);
 
-      player.x = message.pos.x;
-      player.y = message.pos.y;
-      player.dir = message.dir;
-    })
+			player.x = message.pos.x;
+			player.y = message.pos.y;
+			player.dir = message.dir;
+		})
 
-    this.setSimulationInterval(this.update.bind(this))
-  }
+		this.setSimulationInterval(this.update.bind(this))
+	}
 
-    update(deltaTime: number) {
-      this.state.time += deltaTime / 1000;
+	update(deltaTime: number) {
+		this.state.time += deltaTime / 1000;
 
-    }
+	}
 
-  onJoin (client: Client, options: any) {
-    console.log(`${client.sessionId} joined!`, options);
+	onJoin(client: Client, options: any) {
+		console.log(`${client.sessionId} joined!`, options);
 
-    this.state.players.set(client.sessionId, new Player({
-      x: 0,
-      y: 0,
-      name: options.name !== "Guest" ? options.name : generateUsername("-", 3),
-    }));
+		this.state.players.set(client.sessionId, new Player({
+			x: 0,
+			y: 0,
+			name: options.name !== "Guest" ? options.name : generateUsername("-", 3),
+		}));
 
-    if (this.state.leader === undefined) {
-      this.state.leader = client.sessionId;
-    }
-  }
+		if (this.state.leader === undefined) {
+			this.state.leader = client.sessionId;
+		}
+	}
 
-  onLeave (client: Client, consented: boolean) {
-    console.log(client.sessionId, "left!");
+	onLeave(client: Client, consented: boolean) {
+		console.log(client.sessionId, "left!");
 
-    // this.allowReconnection(client, 30);
+		// this.allowReconnection(client, 30);
 
-    if (client.sessionId === this.state.leader) {
-      this.state.leader = Array.from(this.state.players.keys())[0];
-    }
-    
-    this.state.players.delete(client.sessionId);
-  }
+		if (client.sessionId === this.state.leader) {
+			this.state.leader = Array.from(this.state.players.keys())[0];
+		}
 
-  onDispose() {
-    console.log("room", this.roomId, "disposing...");
-  }
+		this.state.players.delete(client.sessionId);
+	}
+
+	onDispose() {
+		console.log("room", this.roomId, "disposing...");
+	}
 
 }
